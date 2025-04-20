59db195d44271ba9c1405316408e1797a002bbd6
Tech Runner By SJ <137867584+techRunnerBySJ@users.noreply.github.com>
2025-04-20 13:25:22 +0530

    feat: update styling in FlipWords component and adjust animation durations in InfiniteMovingCards; add Tooltip component for enhanced UI interactions

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -66,11 +66,11 @@ export const InfiniteMovingCards = ({
   const getSpeed = () => {
     if (containerRef.current) {
       if (speed === "fast") {
-        containerRef.current.style.setProperty("--animation-duration", "1s");
+        containerRef.current.style.setProperty("--animation-duration", "100s");
       } else if (speed === "normal") {
-        containerRef.current.style.setProperty("--animation-duration", "40s");
+        containerRef.current.style.setProperty("--animation-duration", "400s");
       } else {
-        containerRef.current.style.setProperty("--animation-duration", "80s");
+        containerRef.current.style.setProperty("--animation-duration", "100s");
       }
     }
   };

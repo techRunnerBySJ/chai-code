13a32060a9a6514362c6d4f77990bc44f3ce35e2
Tech Runner By SJ <137867584+techRunnerBySJ@users.noreply.github.com>
2025-05-01 12:54:50 +0530

    refactor: update CompaniesSection description to highlight student achievements and startup success

--- src/sections/Companies.tsx
+++ src/sections/Companies.tsx
@@ -96,8 +96,8 @@ function CompaniesSection() {
         </h4>
 
         <p className="text-sm lg:text-base max-w-2xl my-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
-          From Image generation to video generation, Everything AI has APIs for
-          literally everything. It can even create this website copy for you.
+          Our students are not only working in big tech companies, but 
+          are now founders of funded startups and product creators.
         </p>
       </motion.div>
 
